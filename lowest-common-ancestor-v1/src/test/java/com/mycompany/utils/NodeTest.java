13e23bd36fffdca616b0db0cc61d11d8eb056794
claire-1 <claire8991@gmail.com>
2019-09-24 16:40:40 +0100

    Added test for parent with multiple children

--- lowest-common-ancestor-v1/src/test/java/com/mycompany/utils/NodeTest.java
+++ lowest-common-ancestor-v1/src/test/java/com/mycompany/utils/NodeTest.java
@@ -1,20 +1,46 @@
 package com.mycompany.utils;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class NodeTest {
 
     @Test
-    public void givenNewChildNodeShouldAttachChildNodeToParentNode() {
+    public void givenNoChildrenShouldParentShouldReturnEmptyList() {
+        Node parent = new Node(5);
+        assertEquals(0, parent.getChildren().size());
+    }
+    @Test
+    public void givenOneNewChildNodeShouldAttachChildNodeToParentNodeAndChildShouldNotHaveChildren() {
         Node parent = new Node(5);
         Node child = new Node(4);
         parent.addChild(child);
         assertEquals(1, parent.getChildren().size());
         assertEquals(child, parent.getChildren().get(0));
+        assertEquals(0, parent.getChildren().get(0).getChildren().size());
     }
 
-    // TODO add test for more than one node
-    // TODO add test for zero nodes
+    @Test
+    public void givenThreeNewChildNodesShouldAttachChildrenToParentAndAllChildrenShouldNotHaveChildren() {
+        Node parent = new Node(5);
+        Node child1 = new Node(4);
+        Node child2 = new Node(3);
+        Node child3 = new Node(2);
+
+        parent.addChild(child1);
+        parent.addChild(child2);
+        parent.addChild(child3);
+
+        assertEquals(3, parent.getChildren().size());
+        assertTrue(parent.getChildren().contains(child1));
+        assertTrue(parent.getChildren().contains(child2));
+        assertTrue(parent.getChildren().contains(child3));
+
+        assertEquals(0, parent.getChildren().get(0).getChildren().size());
+        assertEquals(0, parent.getChildren().get(1).getChildren().size());
+        assertEquals(0, parent.getChildren().get(2).getChildren().size());
+    }
+
 }
